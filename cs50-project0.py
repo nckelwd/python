8ac4b4e6de1007a755ec6af912f7a51cb2d5bb09
Nick Elwood <elwoodn@me.com>
2023-11-22 08:21:03 -0500

    new practice file cs50

---
+++ cs50-project0.py
@@ -0,0 +1,7 @@
+#indoor voice
+response = str.lower(input("say something quietly: "))
+print(response)
+
+#playback speed
+dotDotDot = str(input("enter three words: ")).split(" ","...")
+print(dotDotDot)
